0433c1fbc70c9220578aa60cd7d02e5a182c92e3
Macbook <macbook@MacBook-Pro-uzivatela-Macbook.local>
2026-07-14 21:54:10 +0200

    Update carbload meal guidance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flegma-tabulky-v42-hourly-water-sound-20260714';
+const CACHE_NAME = 'flegma-tabulky-v43-carbload-macros-20260714';
 const APP_SHELL = [
   './',
   './index.html',
